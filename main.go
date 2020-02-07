4c49e3a518b5434e31021b1fd20e433484b38cc1
RudyRobles <rodolfo_robles@apple.com>
2020-02-06 17:25:15 -0800

    Created a controller for our static pages, and we also updated the View type to implement the http.Handler interface

--- main.go
+++ main.go
@@ -3,35 +3,18 @@ package main
 import (
 	"net/http"
 
-    "github.com/RudePupp/whopays/controllers"
-	"github.com/RudePupp/whopays/views"
+	"github.com/RudePupp/whopays/controllers"
 
 	"github.com/gorilla/mux"
 )
 
-var (
-	homeView    *views.View
-	contactView *views.View
-)
-
-func home(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	must(homeView.Render(w, nil))
-}
-
-func contact(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	must(contactView.Render(w, nil))
-}
-
 func main() {
-	homeView = views.NewView("bootstrap", "views/home.gohtml")
-	contactView = views.NewView("bootstrap", "views/contact.gohtml")
+	staticC := controllers.NewStatic()
 	usersC := controllers.NewUsers()
 
 	r := mux.NewRouter()
-	r.HandleFunc("/", home).Methods("GET")
-	r.HandleFunc("/contact", contact).Methods("GET")
+	r.Handle("/", staticC.Home).Methods("GET")
+	r.Handle("/contact", staticC.Contact).Methods("GET")
 	r.HandleFunc("/signup", usersC.New).Methods("GET")
 	r.HandleFunc("/signup", usersC.Create).Methods("POST")
 	http.ListenAndServe(":3000", r)
